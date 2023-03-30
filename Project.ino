3151530c3ccafd4cc9d2f2fe5a0acfc0bcf9768f
Uygur Tepe <uygur.t10@gmail.com>
2023-03-30 18:44:04 -0400

    code working

--- Project.ino
+++ Project.ino
@@ -3,11 +3,13 @@
 #include <IRremote.h>
 #include <avr/io.h>
 
+int timeruyg = 1;
+
 // Define LCD pins
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 // Define button pin
-const int buttonPin = 6;
+const byte buttonPin = 7;
 
 // Define ultrasonic sensor pins
 const int trigPin = 9;   // Analog Input 0
@@ -23,7 +25,7 @@ void setup() {
   // Set PORT C as 00000000
   DDRC = 0b00000000;
   PORTC = 0b00000000;
-
+  timeruyg = 1;
   // Set button pin as input with internal pull-up resistor
   pinMode(buttonPin, INPUT_PULLUP);
 
@@ -31,6 +33,8 @@ void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   
+  attachInterrupt(digitalPinToInterrupt(buttonPin), buttonInterrupt, CHANGE);
+
   Serial.begin(9600);
   // Initialize LCD
   lcd.begin(16, 2);
@@ -39,13 +43,17 @@ void setup() {
   IrReceiver.begin(IRRecieverPin, ENABLE_LED_FEEDBACK);
 
   // Set up Timer1 for 4 second interrupt
+  cli();
   TCCR1A = 0;
-  TCCR1B = (1 << WGM12) | (1 << CS12) | (1 << CS10);
+  TCCR1B = 0;  //(1 << WGM12) | (1 << CS12) | (1 << CS10);
+  TCNT1 = 0;   //OCR1A = 15624;
   OCR1A = 15624;
-  TIMSK1 = (1 << OCIE1A);
-
+  TCCR1B |= (1 << WGM12);  // Set WGM12 bit to 1, turns on CTC mode:
+  TCCR1B |= (1 << CS12);   // Set CS12 bits to 1
+  /* Notice that When CS10=0, CS11=0 and CS12=1, pre-scaler is 256 */
+  TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt: sei(); // enable global interrupts after configured interrupt
+  sei();
   // Attach interrupt to button pin
-  attachInterrupt(digitalPinToInterrupt(buttonPin), buttonInterrupt, FALLING);
 
   // Set itemDispensed to false at startup
   itemDispensed = false;
@@ -54,10 +62,15 @@ void setup() {
   lcd.print("Choose Drink");
 }
 
-void(* resetFunc) (void) = 0; //declare reset function @ address 0
+ISR(TIMER1_COMPA_vect) {
+  timeruyg++;
+}
+
+void (*resetFunc)(void) = 0;  //declare reset function @ address 0
 
 void loop() {
   if (IrReceiver.decode()) {
+    timeruyg = 0;
     userChoice = int(IrReceiver.decodedIRData.command);
     Serial.println(userChoice);
 
@@ -67,70 +80,60 @@ void loop() {
 
     switch (userChoice) {
       case 12:  // maps to 1
-        // digitalWrite(yellowLedPin, HIGH);
-        // digitalWrite(blueLedPin, LOW);
         PORTC = 0b00000001;
-        lcd.print(1);
+        lcd.print("1");
         break;
       case 24:  // maps to 2
-        // digitalWrite(blueLedPin, HIGH);
-        // digitalWrite(yellowLedPin, LOW);
         PORTC = 0b00000010;
-        lcd.print(2);
+        lcd.print("2");
         break;
       case 94:  // maps to 3
-        // digitalWrite(yellowLedPin, HIGH);
-        // digitalWrite(blueLedPin, LOW);
         PORTC = 0b00000001;
-        lcd.print(3);
+        lcd.print("3");
         break;
       case 8:  // maps to 4
-        // digitalWrite(blueLedPin, HIGH);
-        // digitalWrite(yellowLedPin, LOW);
         PORTC = 0b00000010;
-        lcd.print(4);
+        lcd.print("4");
         break;
       case 28:  // maps to 5
-        // digitalWrite(yellowLedPin, HIGH);
-        // digitalWrite(blueLedPin, LOW);
         PORTC = 0b00000001;
-        lcd.print(5);
+        lcd.print("5");
         break;
       case 90:  // maps to 6
-        // digitalWrite(blueLedPin, HIGH);
-        // digitalWrite(yellowLedPin, LOW);
         PORTC = 0b00000010;
         lcd.print(6);
         break;
       case 66:  // maps to 7
-        // digitalWrite(yellowLedPin, HIGH);
-        // digitalWrite(blueLedPin, LOW);
         PORTC = 0b00000001;
-        lcd.print(7);
+        lcd.print("7");
         break;
       case 82:  // maps to 8
-        // digitalWrite(blueLedPin, HIGH);
-        // digitalWrite(yellowLedPin, LOW);
         PORTC = 0b00000010;
-        lcd.print(8);
+        lcd.print("8");
         break;
       case 74:  // maps to 9
-        // digitalWrite(yellowLedPin, HIGH);
-        // digitalWrite(blueLedPin, LOW);
         PORTC = 0b00000001;
-        lcd.print(9);
+        lcd.print("9");
         break;
       case 22:  // maps to 0
-        // digitalWrite(blueLedPin, HIGH);
-        // digitalWrite(yellowLedPin, LOW);
         PORTC = 0b00000010;
-        lcd.print(0);
+        lcd.print("0");
         break;
     }
+    timeruyg = 0;
+    Serial.println("after time");
+    while (timeruyg <= 16) {
+      //this is a 4 second timer
+      Serial.println("iqdispense");
+      dispenseDrink();
+    }
   }
+  IrReceiver.resume();
 }
 
-ISR(TIMER1_COMPA_vect) {
+
+
+void dispenseDrink() {
   // If 4 seconds have elapsed and item has not been dispensed
   if (!itemDispensed) {
     // Measure distance using ultrasonic sensor
@@ -142,12 +145,9 @@ ISR(TIMER1_COMPA_vect) {
     digitalWrite(trigPin, LOW);
     duration = pulseIn(echoPin, HIGH);
     distance = duration * 0.034 / 2;
-
+    Serial.println(distance);
     // If an object is within 10 cm, set green LED
     if (distance < 10) {
-      // digitalWrite(greenLedPin, HIGH);
-      // digitalWrite(yellowLedPin, LOW);
-      // digitalWrite(blueLedPin, LOW);
       PORTC = 0b00000100;
 
       // Wait for 2 seconds
@@ -160,15 +160,15 @@ ISR(TIMER1_COMPA_vect) {
       resetFunc();  //call reset
     } else {
       // If item has not been dispensed and 4 seconds have elapsed, set red LED
-      // digitalWrite(redLedPin, !digitalRead(redLedPin));
       PORTC = 0b0001000;
       delay(500);
-      PORTC = 0b0000000;      
+      PORTC = 0b0000000;
     }
   }
 }
 
 void buttonInterrupt() {
-  // Read the button input
+  // Read the button
+  Serial.println("inside buttoninterrupt");
   resetFunc();  //call reset
 }
